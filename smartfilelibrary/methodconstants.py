a9337349001d2bd3a83c6efd3bfd3e6c9611e5db
CopperEagle <137458753+CopperEagle@users.noreply.github.com>
2024-05-19 15:08:40 +0200

    Adding constants.

---
+++ smartfilelibrary/methodconstants.py
@@ -0,0 +1,12 @@
+"""Constants for the different extraction methods. Please
+refer to the documentation on what each does and what the requirements
+are."""
+
+## Metadata extraction
+PDFMETA = 1
+DONUTFT = 2
+MOONDREAM2 = 3
+
+## Keywords extraction
+T5DERIVATIVE = 1
+
